7d1f33f8435d2d821a95c79e15da1c8ec5f84792
bnUsmann <bn.usmann22@gmail.com>
2024-06-26 22:51:41 +0100

    rounded up the footer section

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 // notification function
 
-document.getElementById('ntf-show').onclick = function(){
+document.getElementById('ntf-show').onclick = function(event){
+// event.stopPropagation();
+
   var ntfShow = document.getElementById('ntf-txt');
   var descModification = document.getElementById('desc');
 
   ntfShow.classList.toggle('show');
+// notification toggle
+//if (ntfShow.style.display === 'none' || ntfShow.style.display === '' ) {
+//  ntfShow.style.display = 'block';
+//} else {
+// ntfShow.style.display = 'none';
+// }
+//  end of toggle
+
   descModification.classList.toggle('modified');
 
   document.getElementById('ntf-show').classList.add('shake');
@@ -20,4 +30,13 @@ document.getElementById('ntf-show').onclick = function(){
     function() {
       document.getElementById('ntf-show').classList.remove('shake');
     }, 300);
+
+    //hide ntfShow on clicking any other place 
+    // document.addEventListener('click', function(event) {
+    //   var ntfShow = document.getElementById('ntf-txt');
+    //   var showNtf = document.getElementById('ntf-show');
+    //   if(!ntfShow.contains(event.target) && !showNtf.contains(event.target)){
+    //     ntfShow.style.display = 'none';
+    //   }
+    // })
 };
