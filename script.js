7ff74d24846930a8e550557f71a38801e3db49e3
bnUsmann <bn.usmann22@gmail.com>
2024-06-26 16:59:19 +0100

    created the toggle for the notification SVG & also created and styled the overflow of the notification

--- script.js
+++ script.js
@@ -8,6 +8,14 @@ document.getElementById('ntf-show').onclick = function(){
   descModification.classList.toggle('modified');
 
   document.getElementById('ntf-show').classList.add('shake');
+   //toggling the svg color
+  if (document.getElementById('ntf-show').classList.contains('green')) {
+    document.getElementById('ntf-show').classList.remove('green')
+    document.getElementById('ntf-show').classList.add('white')
+  } else {
+    document.getElementById('ntf-show').classList.remove('white')
+    document.getElementById('ntf-show').classList.add('green')
+  }
   setTimeout(
     function() {
       document.getElementById('ntf-show').classList.remove('shake');
