b2d0c56f8bdae24e39fe3a59c959dcbd378f6093
Jamil Muhammad, ABDULLAHI <130340582+bnusmann22@users.noreply.github.com>
2024-11-29 06:24:40 +0100

    Update App.jsx
    ...

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,18 @@ import { lazy } from "react";
 
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-const Homepage = lazy(() => import("./pages/Homepage"));
+import Homepage from "./pages/Homepage"
 
 function App() {
 	return (
-		<BrowserRouter>
-			<Routes>
-				<Route index element={<Homepage/>} />
-			</Routes>
-		</BrowserRouter>
+		//<BrowserRouter>
+			//<Routes>
+	//	<Route index element={<Homepage/>} />
+	//</Routes>
+		//</BrowserRouter>
+		<>
+		 <Homepage />
+		</>
 	);
 }
 
